ce8c8db78a99da616ddc71a3cedd70029a7d35c3
1solovedeng <244661478@qq.com>
2025-05-11 15:13:04 +0800

    V1

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <SDL.h>
+
+int main(int, char**) {
+    std::cout << "Hello, World!" << std::endl;
+    // SDL初始化
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;}
+    
+    //创建窗口
+    SDL_Window *window = SDL_CreateWindow("Hello World!", 100, 100, 800, 600, SDL_WINDOW_SHOWN);
+
+    //创建渲染器
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    
+    // Do Something with the window and renderer
+    //渲染循环
+    while (true) {
+        SDL_Event event;
+        if (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                break;
+            }
+        }
+        
+        //清屏 - 会用画笔颜色清空
+        SDL_RenderClear(renderer);
+        //渲染
+        SDL_Rect rect = {100, 100, 200, 200};
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_RenderFillRect(renderer, &rect);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+        //更新屏幕
+        SDL_RenderPresent(renderer);
+    }
+
+    //清理并退出
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
